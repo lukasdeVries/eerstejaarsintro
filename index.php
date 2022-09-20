5c143c11ccd58a235c2b8f76672f567b167bed02
lukasdeVries <lukaspdevries@gmail.com>
2022-09-20 10:04:44 +0200

    upload files
    alle bestanden en mappen toegeveoegd die ik al had gemaakt. homepage en style sheat en een aantal afbeeldingen

---
+++ index.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>eerstejaarsIntro</title>
+    <link rel="stylesheet" href="css/main.css">
+</head>
+<body>
+    <header>
+        <div class="wrapper">
+            <div class="logo">
+                <img src="img/curioBannerYellow.png" alt="curio">
+            </div>
+            <div class="nameAndYear">
+                <h1>Lukas de Vries</h1>
+                <h3>2e jaar</h3>
+            </div>
+        </div>
+    </header>
+    <main>
+        <div class="wrapper">
+            <div class="classesBox">
+                <div class="class">
+                    <img src="img/web.jpg" alt="web">
+                    <p>In de web lessen leer je alles over hoe je een website kan maken en vormgeven. In je eerste jaar ga je veel bezig zijn met css en html om basis sites te maken.</p>
+                </div>
+                <div class="class">
+                    <img src="img/pra.jpeg" alt="pra">
+                    <p>In de web lessen leer je alles over hoe je een website kan maken en vormgeven. In je eerste jaar ga je veel bezig zijn met css en html om basis sites te maken. 
+                    </p>
+                </div>
+                <div class="class">
+                    <img src="img/pro.jpg" alt="pro">
+                    <p>In de pro lessen leer je alles over hoe je programeer projecten aan kunt pakken en ontwikkelen. Denk hierbij bijvoorbeeld aan hoe je een basis ontwerp maakt, hoe je effectief een taakverdeling maakt of hoe je de functionaliteit van een programma makkelijk op een rijtje zet. </p>
+                </div>
+                <div class="class">
+                    <img src="img/native.webp" alt="native">
+                    <p>Met native lessen ga je leren over programmeren in talen zoals python en c#. Tijdens deze lessen leer je logica toe te passen in code en hoe je dit moet toepassen.</p>
+                </div>
+            </div><!--classboxes-->
+            <div class="classStorys">
+                <div class="story">
+                    <h2>roosters</h2>
+                    <p>Je rooster veranderd ongeveer om de 8 weken. Van de drie
+                        hoofdvakken (native, web en pra) heb je meestal 2 lessen van 2uur
+                        verspreid over de week. Als er weinig lessen op een dag zijn is
+                        er een grote kans dat de lessen online worden gegeven. In mijn
+                        eerste jaar waren er een aantal periodes waarop ik een vaste
+                        dag vrij had of kort online lessen. Hieronder staat mijn
+                        huidige rooster. </p>
+                </div>
+                <div class="story">
+                    <h2>blokken</h2>
+                    <p>Een schooljaar is verdeeld in twee blokken, in het eerste blok leg
+                        je een hoop van de basis en wordt er gekeken naar hoe je de stof
+                        oppakt en of je mogelijk kan versnellen. In het tweede blok kan
+                        je dus ook een nieuwe klas krijgen afhankelijk van wie er gaat
+                        versnellen en wie niet. Aan het eind van elk blok worden er
+                        toetsen afgenomen voor web en native om te kijken of je verder 
+                        kan op hetzelfde traject en of er dingen zijn waar je eventueel 
+                        meer aandacht aan moet geven.</p>
+                </div>
+                <div class="story">
+                    <h2>Versnellen</h2>
+                    <p>Als je in het eerste blok meer dan 90% van al je leerdoelen bereikt
+                        kan je ervoor kiezen om te versnellen. Het verschil tussen het
+                        versnelde traject en het gewone is dat je vanaf het tweede blok
+                        het extra vak delta krijgt. Bij delta krijg je stof die je 
+                        normaal gesproken pas later in je traject zou krijgen bovenop
+                        alle lessen die je in het andere traject ook al zou krijgen.
+                        Omdat je op een hoger tempo door de stof heen gaat kan je
+                        ook een halfjaar eerder op stage. De versnellende klas gaat
+                        in het vierde blok op stage waar de andere klassen in het 
+                        vierde jaar gaan.</p>
+                </div>
+            </div><!--classStorys-->
+        </div><!--wrapper-->
+        <div class="scheduleBanner">
+            <img src="img/rooster.png" alt="rooster">
+        </div><!--scheduleBanner-->
+    </main>
+    </body>
+</html>
